07fc2aca0d402d423c412219ed64b494c108999f
Jelena Stevanovic <jstevanovic318@gmail.com>
2022-10-20 11:53:35 +0200

    dodata klasa usre i login radi

---
+++ model/users.php
@@ -0,0 +1,24 @@
+<?php
+    class User{
+        public $id;
+        public $username;
+        public $password;
+
+        public function __construct($id = null, $username = null, $password = null)
+        {
+            $this->id = $id;
+            $this->username = $username;
+            $this->password = $password;
+        }
+        
+        public static function loginUser($user, mysqli $conn)
+        {
+            $query = "SELECT * FROM user WHERE username='$user->username' AND 
+            password = '$user->password'";
+           // return true;
+            return $conn -> query($query);
+        }
+
+
+    }
+?>
